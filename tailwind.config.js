1e266e3b7e69bd864a19975ce1cd1ca8e455568f
RussBeechly <russ.beechly@samlanders.com>
2025-07-23 09:32:46 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./styles/**/*.{css}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
 
 
