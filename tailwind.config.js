17b74720ee189bd0c075aeda4f67fda9ef48bd3c
RussBeechly <russ.beechly@samlanders.com>
2025-07-23 09:24:54 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}", // Looks in pages directory
-    "./components/**/*.{js,ts,jsx,tsx}", // If you add components later
-    "./app/**/*.{js,ts,jsx,tsx}" // If you ever use Next.js /app directory
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
 
 
