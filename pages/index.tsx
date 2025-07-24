34a5502a868f95e0e98d8f8510d3b6166a66b344
RussBeechly <russ.beechly@samlanders.com>
2025-07-24 14:36:05 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,10 +35,14 @@ export default function Home() {
   };
 
   const updateItem = (index: number, key: keyof Item, value: string | number) => {
-    const updated = [...items];
-    updated[index][key] = key === "price" ? Number(value) : (value as string);
-    setItems(updated);
+  const updated = [...items];
+  updated[index] = {
+    ...updated[index],
+    [key]: key === "price" ? Number(value) : (value as string),
   };
+  setItems(updated);
+};
+
 
   const deleteItem = (index: number) => {
     setItems(items.filter((_, i) => i !== index));
